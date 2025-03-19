4b76c593079c91a2d1c9e0a2d30a8c82fbab88e8
CSiron21 <csiron9029@gmail.com>
2025-03-19 20:24:26 +0800

    Add files via upload

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -4,6 +4,20 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
   networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: "http://127.0.0.1:8545",
+        blockNumber: 0
+      }
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk"
+      }
+    },
     ganache: {
       url: "http://127.0.0.1:7545",  // This is the default Ganache URL
       chainId: 1337,
